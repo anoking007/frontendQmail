c0752ec533f4b8d1fa9093c844dec0a643d0ecba
Aman8427 <amankandagal18@gmail.com>
2024-03-21 22:17:11 +0530

    comment route

--- routes/Dashboard.jsx
+++ routes/Dashboard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from '../component/Header'
 
 export const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+  return (<><div>
+    <Header/>
+</div></>
+    
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Dashboard } from '../routes/Dashboard'
 
 
 function App() {
@@ -9,10 +10,10 @@ function App() {
   <>
   <BrowserRouter>
   <Routes>
-  <Route path="/signup" element={<SignUp></SignUp>}></Route>
-  <Route path="/signin" element={<SignIn></SignIn>}></Route>
+  {/* <Route path="/signup" element={<SignUp></SignUp>}></Route>
+  <Route path="/signin" element={<SignIn></SignIn>}></Route> */}
   <Route path='/maildashboard' element={<Dashboard></Dashboard>}></Route>
-  <Route path='/sendmail' element={<SendMail></SendMail>}></Route>
+  {/* <Route path='/sendmail' element={<SendMail></SendMail>}></Route> */}
 
   </Routes>
   
